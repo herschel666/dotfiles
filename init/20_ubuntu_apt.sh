9aa4137679d98c64acfbf1c3d2e8f017864e5ef8
herschel666 <kraemerseele@gmx.de>
2018-10-07 21:11:27 +0200

    Install gvim

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -79,6 +79,12 @@ apt_packages+=(ansible)
 # http://tipsonubuntu.com/2016/09/13/vim-8-0-released-install-ubuntu-16-04/
 add_ppa ppa:jonathonf/vim
 apt_packages+=(vim)
+apt_packages+=(vim-gtk3)
+apt_packages+=(vim-tiny)
+apt_packages+=(neovim-qt)
+apt_packages+=(vim-athena)
+apt_packages+=(vim-gtk)
+apt_packages+=(vim-nox)
 
 # https://launchpad.net/~hnakamur/+archive/ubuntu/tmux
 add_ppa ppa:hnakamur/tmux
