2809bc213b418e5c9cc76fa81e74daf518b87b4b
herschel666 <kraemerseele@gmx.de>
2018-10-07 20:33:17 +0200

    Be more specific when adding SS keys

--- source/00_ssh.sh
+++ source/00_ssh.sh
@@ -13,9 +13,9 @@ agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
 
 if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
     agent_start
-    ssh-add
+    ssh-add ~/.dotfiles/link/.ssh/id_*
 elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-    ssh-add
+    ssh-add ~/.dotfiles/link/.ssh/id_*
 fi
 
 unset env
