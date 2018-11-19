9fe4e6f36d5f64c945a4ed0eeea2b6128b5b4722
herschel666 <kraemerseele@gmx.de>
2018-11-19 18:52:01 +0100

    Remove unnecessary casks

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -21,43 +21,20 @@ casks=(
   a-better-finder-rename
   alfred
   android-platform-tools
-  bartender
-  battle-net
-  bettertouchtool
   charles
   chromium
-  chronosync
-  controllermate
   docker
   dropbox
-  fastscripts
   firefox
   gyazo
-  hex-fiend
   iterm2
-  karabiner-elements
-  macvim
-  messenger-for-desktop
   midi-monitor
-  moom
   omnidisksweeper
-  race-for-the-galaxy
-  reaper
   robo-3t
-  screenhero
-  scroll-reverser
-  skype
   slack
-  sourcetree
   spotify
-  steam
   the-unarchiver
-  totalfinder
-  tower
-  vagrant
-  virtualbox
   vlc
-  ynab
   # Quick Look plugins
   betterzipql
   qlcolorcode
@@ -72,9 +49,6 @@ casks=(
   # Color pickers
   colorpicker-developer
   colorpicker-skalacolor
-  # Drivers
-  sonos
-  xbox360-controller-driver
   # Fonts
   font-m-plus
   font-mplus-nerd-font
