d5b9915f3b7a337a439edb2c11cd2b0b359d5077
herschel666 <kraemerseele@gmx.de>
2018-11-20 11:43:05 +0100

    use $DOTFILES variable

--- init/50_node.sh
+++ init/50_node.sh
@@ -3,5 +3,5 @@ source $DOTFILES/source/50_node.sh init
 
 if [[ ! "$(type -P nvm)" ]]; then
   e_header "Installing nvm"
-  source ../vendor/nvm/install.sh
+  source $DOTFILES/vendor/nvm/install.sh
 fi
