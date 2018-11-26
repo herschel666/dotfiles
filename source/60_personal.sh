12b2a7d863652ad56bf158c860b537a343bbdd6b
herschel666 <kraemerseele@gmx.de>
2018-11-26 08:38:19 +0100

    add code command to PATH

--- source/60_personal.sh
+++ source/60_personal.sh
@@ -2,6 +2,7 @@
 # export PATH="$HOME/.oly/bin:$PATH"
 # export DOCKER_HOST=tcp://0.0.0.0:2375
 export GPG_TTY=$(tty)
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 function cd {
     builtin cd "$@"
